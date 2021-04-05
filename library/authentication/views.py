a1e93e3bd83d48c51caf63db2f2ac327f8840e3c
vooply <pv.yaschuk@gmail.com>
2021-04-06 02:03:04 +0300

    pep8 views

--- library/authentication/views.py
+++ library/authentication/views.py
@@ -1,15 +1,6 @@
-from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import status
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 
 from authentication.models import CustomUser
 from authentication.serializers import UserSerializer, RegistrationSerializer
@@ -33,20 +24,6 @@ class CreateUserView(CreateAPIView):
     queryset = CustomUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
-
-
-# class LoginAPIView(APIView):
-#     authentication_classes = (SessionAuthentication, BasicAuthentication)
-#
-#     # permission_classes = (IsAuthenticated,)
-#
-#     def get(self, request, format=None):
-#         content = {
-#             'user': str(request.user),
-#             'auth': str(request.auth),
-#         }
-#         return Response(content)
-
 
 
 def logout(request):
