38bfc34cc33cf1af354bcab21a09597080cae33d
vooply <pv.yaschuk@gmail.com>
2021-04-05 17:17:27 +0300

    add AUTH rest to settings

--- library/library/settings.py
+++ library/library/settings.py
@@ -110,7 +110,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
